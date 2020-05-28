4be69f9348462ed390212458d60fc2d903fc6e81
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2020-05-28 18:23:22 +0200

    Working 1czy full example with ANM and restraints

---
+++ example/1czy/execution.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Edit only the number of cores to use for this simulation
+NUM_CORES=4
+
+# LightDock setup
+lightdock3_setup.py 1czy_protein.pdb 1czy_peptide.pdb 400 200 --noxt --noh -anm -rst restraints.list
+
+# Convert ANM data
+lgd_flatten.py lightdock_rec.nm.npy rec_nm.npy
+lgd_flatten.py lightdock_lig.nm.npy lig_nm.npy
+
+# Calculate number of swarms
+s=`ls -d swarm_* | wc -l`
+swarms=$((s-1))
+
+# Create a task.list file for ant_thony
+for i in `seq 0 $swarms`;do echo "cd swarm_${i}; cp ../lightdock_1czy_protein.pdb .; cp ../lightdock_1czy_peptide.pdb .;cp ../rec_nm.npy .;cp ../lig_nm.npy .;../lightdock-rust ../setup.json ../init/initial_positions_${i}.dat 10; rm -rf lightdock_*.pdb *.npy;" >> task.list; done
+
+# Let ant_thony run
+ant_thony.py --cores ${NUM_CORES} task.list
+
