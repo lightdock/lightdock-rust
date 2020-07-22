34bd2efc35c420c28e0576d27c73e42f2929bf73
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2020-07-22 16:38:01 +0200

    Fixed 1czy example

--- example/1czy/execution.sh
+++ example/1czy/execution.sh
@@ -14,9 +14,14 @@ lgd_flatten.py lightdock_lig.nm.npy lig_nm.npy
 s=`ls -d swarm_* | wc -l`
 swarms=$((s-1))
 
+# Copy binary
+cp ../../target/release/lightdock-rust .
+
 # Create a task.list file for ant_thony
-for i in `seq 0 $swarms`;do echo "cd swarm_${i}; cp ../lightdock_1czy_protein.pdb .; cp ../lightdock_1czy_peptide.pdb .;cp ../rec_nm.npy .;cp ../lig_nm.npy .;../lightdock-rust ../setup.json ../init/initial_positions_${i}.dat 10; rm -rf lightdock_*.pdb *.npy;" >> task.list; done
+for i in `seq 0 $swarms`;do echo "cd swarm_${i}; cp ../lightdock_1czy_protein.pdb .; cp ../lightdock_1czy_peptide.pdb .;cp ../rec_nm.npy .;cp ../lig_nm.npy .;cp -R ../../../data .;../lightdock-rust ../setup.json ../init/initial_positions_${i}.dat 10 dfire; rm -rf lightdock_*.pdb *.npy data;" >> task.list; done
 
 # Let ant_thony run
 ant_thony.py --cores ${NUM_CORES} task.list
 
+# Clean task.list
+rm -rf task.list
